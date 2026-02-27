55efe7f4e9a5e6f87bd0d459fb65de4bb2db936e
Gabrielmachry <104443297+Gabrielmachry@users.noreply.github.com>
2026-02-27 03:06:11 -0300

    Refactor baixarPDF function for PDF generation
    Updated PDF generation function to select a specific element and adjusted options for margin and unit.

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
 function baixarPDF() {
 
-    const elemento = document.body;
+    const element = document.querySelector(".page");
 
     const opt = {
-        margin: 0.5,
+        margin: 5,
         filename: 'Gabriel_Machry_Curriculo.pdf',
         image: { type: 'jpeg', quality: 1 },
-        html2canvas: { scale: 2, useCORS: true },
-        jsPDF: { unit: 'in', format: 'a4', orientation: 'portrait' }
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
     };
 
-    // Esconde botões antes de gerar
     const botoes = document.querySelectorAll('.no-print');
     botoes.forEach(btn => btn.style.display = "none");
 
     html2pdf()
         .set(opt)
-        .from(elemento)
+        .from(element)
         .save()
         .then(() => {
-            // Mostra os botões novamente
             botoes.forEach(btn => btn.style.display = "inline-block");
         });
-
 }
